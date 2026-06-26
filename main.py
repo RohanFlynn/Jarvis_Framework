4aa9966e78f810a7244472a93d6d68fac87dd63a
rohan flynn <rohanjflynn@gmail.com>
2026-06-26 19:32:01 +1000

    worked on app design

--- main.py
+++ main.py
@@ -3,14 +3,29 @@
 #|====================|#
 
 import customtkinter as tk
+from listerner import J_listen
 root = tk.CTk()
 
 root.geometry("400x400")
 root.config(bg="#2045A8")
 
-def start_listen():
-    pass
+#variables
+on = False
 
-start_listener = tk.CTkButton(root, text="Start", command=start_listen)
-start_listener.place(x=140, y=250)
+def l_check():
+    while on:
+        J_listen.Listen()
+
+def start_listen():
+    global on
+    if not on:
+        start_listener.configure(text="Stop", fg_color="red", hover_color="red")
+        on = True
+    else:
+        start_listener.configure(text="Start", fg_color="green", hover_color="green")
+        on = False
+    l_check()
+
+start_listener = tk.CTkButton(root, text="Start", command=start_listen, fg_color="green", hover_color="green", border_width=1, border_color="black")
+start_listener.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
 root.mainloop()
